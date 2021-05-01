cd7cf5f99b2d2692d2380f5c94a08f87f4318e80
Paul Krybus <paul.krybus@yahoo.de>
2021-05-01 17:51:23 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div><p>Hello {props.name}, it is {props.now}!</p></div>
+  )
+};
+
+const App = () => {
+  let x = 2;
+  return (
+    <div><h1>Greetings</h1>
+      <Hello name='Paul' now={new Date().toString()} />
+      <Hello name='Daisy' now={new Date().toString()} />
+      <Hello name='Jim' now={new Date().toString()} />
     </div>
-  );
-}
+  )
+};
 
 export default App;
